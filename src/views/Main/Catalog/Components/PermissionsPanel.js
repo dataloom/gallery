9adbe10daa7484896d9d6388e8b6e0f87782cf79
Katherine <katherine@kryptnostic.com>
2016-10-27 10:58:23 -0700

    show roles and emails

--- src/views/Main/Catalog/Components/PermissionsPanel.js
+++ src/views/Main/Catalog/Components/PermissionsPanel.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { Button } from 'react-bootstrap';
 import Dropdown from 'react-dropdown';
 import { EntityDataModelApi } from 'loom-data';
+import Utils from '../../../../utils/Utils';
 import Consts from '../../../../utils/AppConsts';
 import styles from '../styles.module.css';
 import '../../../../styles/dropdown.css';
@@ -13,11 +14,23 @@ const views = {
   EMAILS: 3
 };
 
-const accessOptions = [
-  'Hidden',
-  'Discoverable',
-  'Public'
-];
+const accessOptions = {
+  Hidden: 'Hidden',
+  Discoverable: 'Discoverable',
+  Public: 'Public'
+};
+
+const emails = {
+  Hidden: ['first@hidden.com', 'second@hidden.com', 'third@hidden.com', 'fourth@hidden.com', 'fifth@hidden.com', 'sixth@hidden.com', 'seventh@hidden.com', 'eighth@hidden.com', 'ninth@hidden.com', 'tenth@hidden.com', 'eleventh@hidden.com'],
+  Discoverable: ['one@discoverable.com', 'two@discoverable.com', 'three@discoverable.com'],
+  Public: ['heresAnEmail@public.com']
+};
+
+const roles = {
+  Hidden: ['role1', 'role2', 'role3'],
+  Discoverable: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  Public: ['Software engineer', 'Product manager', 'BD', 'some job']
+};
 
 export class PermissionsPanel extends React.Component {
   static propTypes = {
@@ -33,7 +46,9 @@ export class PermissionsPanel extends React.Component {
     this.state = {
       view: views.GLOBAL,
       updateSuccess: false,
-      updateError: false
+      updateError: false,
+      rolesView: accessOptions.Public,
+      emailsView: accessOptions.Public
     };
   }
 
@@ -85,6 +100,10 @@ export class PermissionsPanel extends React.Component {
     this.setState({ updateSuccess: true });
   }
 
+  buttonStyle = (view, viewState) => {
+    return (view === viewState) ? styles.selectedButtonStyle : styles.buttonStyle;
+  }
+
   getGlobalView = () => {
     return (
       <div className={styles.viewWrapper}>
@@ -92,9 +111,9 @@ export class PermissionsPanel extends React.Component {
         <div className={styles.spacerSmall} />
         <div className={styles.dropdownWrapper}>
           <Dropdown
-            options={accessOptions}
+            options={Object.keys(accessOptions)}
             onChange={this.onSelect}
-            value={accessOptions[0]}
+            value={Object.keys(accessOptions)[0]}
           />
         </div>
         <div className={styles.spacerSmall} />
@@ -103,9 +122,60 @@ export class PermissionsPanel extends React.Component {
     );
   }
 
+  changeRolesView = (newView) => {
+    this.setState({ rolesView: newView });
+  }
+
+  removeRole = (role) => {
+    console.log(`removing ${role} from ${this.state.rolesView} list`);
+  }
+
+  addRole = () => {
+    const role = document.getElementById('newRole').value;
+    const view = this.state.rolesView;
+    console.log(`adding ${role} to ${view} role list`);
+    this.setState({ updateSuccess: true });
+  }
+
   getRolesView = () => {
+    const roleList = roles[this.state.rolesView];
+    const hiddenBody = roleList.map(role => <div className={styles.tableRows}>
+      <div style={{ display: 'inline' }}>
+        <Button bsSize="xsmall" bsStyle="danger" onClick={() => this.removeRole(role)}>-</Button>
+      </div>
+      <div style={{ display: 'inline', paddingLeft: '10px' }}>{role}</div>
+    </div>);
     return (
-      <div>roles view</div>
+      <div>
+        <div>Choose levels of access for specific roles.</div>
+        <div style={{ display: 'inline-block', paddingTop: '10px' }}>
+          <button
+            onClick={() => this.changeRolesView(accessOptions.Public)}
+            className={this.buttonStyle(accessOptions.Public, this.state.rolesView)}
+          >{accessOptions.Public}</button>
+          <button
+            onClick={() => this.changeRolesView(accessOptions.Discoverable)}
+            className={this.buttonStyle(accessOptions.Discoverable, this.state.rolesView)}
+          >{accessOptions.Discoverable}</button>
+          <button
+            onClick={() => this.changeRolesView(accessOptions.Hidden)}
+            className={this.buttonStyle(accessOptions.Hidden, this.state.rolesView)}
+          >{accessOptions.Hidden}</button>
+        </div>
+        <div style={{ margin: '0 auto', height: '200px', width: '300px', overflow: 'scroll', textAlign: 'left', paddingTop: '5px' }}>
+          {hiddenBody}
+        </div>
+        <div style={{ display: 'inline-block' }}>
+          <input
+            id="newRole"
+            type="text"
+            style={{ width: '200px' }}
+            placeholder={'Enter a new role'}
+            className={styles.inputBox}
+          />
+          <Button className={styles.spacerMargin} onClick={this.addRole}>Save</Button>
+        </div>
+      </div>
     );
   }
 
@@ -116,9 +186,9 @@ export class PermissionsPanel extends React.Component {
         <div className={styles.spacerSmall} />
         <div className={styles.dropdownWrapper}>
           <Dropdown
-            options={accessOptions}
+            options={Object.keys(accessOptions)}
             onChange={this.onSelect}
-            value={accessOptions[0]}
+            value={Object.keys(accessOptions)[0]}
           />
         </div>
         <div className={styles.spacerSmall} />
@@ -127,9 +197,60 @@ export class PermissionsPanel extends React.Component {
     );
   }
 
+  changeEmailsView = (newView) => {
+    this.setState({ emailsView: newView });
+  }
+
+  removeEmail = (email) => {
+    console.log(`removing ${email} from ${this.state.emailsView} list`);
+  }
+
+  addEmail = () => {
+    const email = document.getElementById('newEmail').value;
+    const view = this.state.emailsView;
+    console.log(`adding ${email} to ${view} email list`);
+    this.setState({ updateSuccess: true });
+  }
+
   getEmailsView = () => {
+    const emailList = emails[this.state.emailsView];
+    const hiddenBody = emailList.map(email => <div className={styles.tableRows}>
+      <div style={{ display: 'inline' }}>
+        <Button bsSize="xsmall" bsStyle="danger" onClick={() => this.removeEmail(email)}>-</Button>
+      </div>
+      <div style={{ display: 'inline', paddingLeft: '10px' }}>{email}</div>
+    </div>);
     return (
-      <div>emails view</div>
+      <div>
+        <div>Choose levels of access for specific email addresses.</div>
+        <div style={{ display: 'inline-block', paddingTop: '10px' }}>
+          <button
+            onClick={() => this.changeEmailsView(accessOptions.Public)}
+            className={this.buttonStyle(accessOptions.Public, this.state.emailsView)}
+          >{accessOptions.Public}</button>
+          <button
+            onClick={() => this.changeEmailsView(accessOptions.Discoverable)}
+            className={this.buttonStyle(accessOptions.Discoverable, this.state.emailsView)}
+          >{accessOptions.Discoverable}</button>
+          <button
+            onClick={() => this.changeEmailsView(accessOptions.Hidden)}
+            className={this.buttonStyle(accessOptions.Hidden, this.state.emailsView)}
+          >{accessOptions.Hidden}</button>
+        </div>
+        <div style={{ margin: '0 auto', height: '200px', width: '300px', overflow: 'scroll', textAlign: 'left', paddingTop: '5px' }}>
+          {hiddenBody}
+        </div>
+        <div style={{ display: 'inline-block' }}>
+          <input
+            id="newEmail"
+            type="text"
+            style={{ width: '200px' }}
+            placeholder={'Enter a new email address'}
+            className={styles.inputBox}
+          />
+          <Button className={styles.spacerMargin} onClick={this.addEmail}>Save</Button>
+        </div>
+      </div>
     );
   }
 
