b880f812c7385fd0fb23d734d37e1a9e2b3f1bd4
Katherine <katherine@kryptnostic.com>
2016-10-31 09:48:36 -0700

    display mock data

--- src/views/Main/Home/Home.js
+++ src/views/Main/Home/Home.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import styles from './styles.module.css';
+
+const requests = [
+  {
+    email: 'givemeaccesspls@gmail.com',
+    dataset: 'MHS Data',
+    msg: 'Hi! I would like to have access to your dataset please. It would be very helpful to me to have access to this thing for a variety of interesting and convincing reasons! Give it to me please.',
+    time: Date.now()
+  },
+  {
+    email: 'katherine@kryptnostic.com',
+    dataset: 'Kryptnostic',
+    msg: 'please let me see the data :)',
+    time: Date.now()
+  }
+];
 
 export class Home extends React.Component {
 
   render() {
+
+    const allRequests = requests.map((request) => {
+      return (
+        <div>
+          <div>{request.time}</div>
+          <div>{request.email} requests access to {request.dataset}</div>
+          <div>{request.msg}</div>
+          <div>
+            <Button>Approve</Button>
+            <Button>Deny</Button>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div>
         <div>
           <div className={styles.sectionHeader}>Pending Action Items</div>
+          {allRequests}
         </div>
         <div className={styles.spacer} />
         <div>
