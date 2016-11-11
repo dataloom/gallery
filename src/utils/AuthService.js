e817ea4f91c69b98c0a46da2ac5792b6bd8d56e4
Hristo Oskov <hristo@kryptnostic.com>
2016-11-10 19:57:01 -0800

    setting the authorization cookie

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -1,5 +1,9 @@
-import { EventEmitter } from 'events';
 import Auth0Lock from 'auth0-lock';
+
+import * as Cookies from 'js-cookie';
+
+import { EventEmitter } from 'events';
+
 import { isTokenExpired } from './jwtHelper';
 
 export default class AuthService extends EventEmitter {
@@ -62,6 +66,8 @@ export default class AuthService extends EventEmitter {
   setToken(idToken) {
     // Saves user token to localStorage
     this.storage.setItem('id_token', idToken);
+
+    Cookies.set('authorization', `Bearer ${idToken}`);
   }
 
   getToken() {
@@ -73,5 +79,7 @@ export default class AuthService extends EventEmitter {
     // Clear user token and profile data from localStorage
     this.storage.removeItem('id_token');
     this.storage.removeItem('profile');
+
+    Cookies.remove('authorization');
   }
 }
