21bea3c40c299cb343d43fdd1b59e6812b9a18e6
Katherine <katherine@kryptnostic.com>
2016-12-07 15:34:17 -0800

    bound offset const, better naming

--- src/utils/Consts/VisualizationConsts.js
+++ src/utils/Consts/VisualizationConsts.js
@@ -1,6 +1,8 @@
 const LATITUDE = 'latitude';
 const LONGITUDE = 'longitude';
 
+const DEFAULT_BOUND_OFFSET = 0.01;
+
 const LINE_CHART = 'Line Chart Visualization';
 const SCATTER_CHART = 'Scatter Chart Visualization';
 const MAP_CHART = 'Map Visualization';
@@ -8,6 +10,8 @@ const MAP_CHART = 'Map Visualization';
 export default {
   LATITUDE,
   LONGITUDE,
+
+  DEFAULT_BOUND_OFFSET,
 
   LINE_CHART,
   SCATTER_CHART,
