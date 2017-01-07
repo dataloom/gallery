f270df8eb2e6ea5a34870a4f39cdca9a146863ff
Arion Sprague <arion@kryptnostic.com>
2017-01-06 19:34:29 -0800

    Moved permissions to enumify

--- src/utils/Consts/PermissionsConsts.js
+++ src/utils/Consts/PermissionsConsts.js
@@ -1,23 +1,47 @@
-const HIDDEN = 'HIDDEN';
-const DISCOVER = 'DISCOVER';
-const READ = 'READ';
-const WRITE = 'WRITE';
-const OWNER = 'OWNER';
+// @flow
 
-const ADD = 'ADD';
-const REMOVE = 'REMOVE';
-const SET = 'SET';
-const REQUEST = 'REQUEST';
+import { Enum } from 'enumify';
+
+function enumFactory(friendlyName) {
+  return {
+    get friendlyName() {
+      return friendlyName;
+    },
+    hasPermission(permission: Permission) {
+      return this.ordinal >= permission.ordinal;
+    }
+  };
+}
+
+class Permission extends Enum {}
+Permission.initEnum({
+  'HIDDEN': enumFactory("Hidden"),
+  'DISCOVER': enumFactory("Discover"),
+  'READ': enumFactory("Read"),
+  'WRITE': enumFactory("Write"),
+  'OWNER': enumFactory("Owner")
+});
+
+const ALL_PERMISSIONS = Array.from(Permission);
+
+function maxPermission(permissions: Permission) {
+  let highestPerm = Permission.HIDDEN;
+
+  for (let permission of permissions) {
+    if (permission.ordinal > highestPerm.ordinal) {
+      highestPerm = permission;
+    }
+  }
+}
 
 export default {
-  HIDDEN,
-  DISCOVER,
-  READ,
-  WRITE,
-  OWNER,
+  HIDDEN: Permission.HIDDEN,
+  DISCOVER: Permission.DISCOVER,
+  READ: Permission.READ,
+  WRITE: Permission.WRITE,
+  OWNER: Permission.OWNER,
+  ALL_PERMISSIONS,
 
-  ADD,
-  REMOVE,
-  SET,
-  REQUEST
-};
+  Permission,
+  maxPermission
+}
