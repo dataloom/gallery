28eaaeb3466f68bc7f75c77cd13c8979bda31461
Arion Sprague <arion@kryptnostic.com>
2017-01-08 20:15:55 -0800

    Added EntitySetList styles

--- src/components/entityset/EntitySet.js
+++ src/components/entityset/EntitySet.js
@@ -6,13 +6,55 @@ import { DropdownButton, Button, MenuItem } from 'react-bootstrap';
 
 import { DataApi, EntityDataModelApi, PermissionsApi } from 'loom-data';
 
-import { PropertyList } from '../propertylist/PropertyList';
-import { PermissionsPanel } from '../permissions/PermissionsPanel';
-import StringConsts from '../../utils/Consts/StringConsts';
 import FileConsts from '../../utils/Consts/FileConsts';
 import PageConsts from '../../utils/Consts/PageConsts';
-import { Permission } from '../../core/permissions/Permission';
-import styles from '../../containers/catalog/catalog.module.css';
+import styles from './entityset.module.css';
+
+const MAX_DESCRIPTION_LENGTH = 300;
+
+class ExpandableText extends React.Component {
+  static propTypes = {
+    text: PropTypes.string,
+    maxLength: PropTypes.number
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+
+    this.switchState = () => {
+      this.setState({ isOpen: !this.state.isOpen });
+    }
+  }
+
+  render() {
+    let {text, maxLength} = this.props;
+    if (text.length <= maxLength) {
+      return text;
+    }
+
+    let {isOpen} = this.state,
+      controlText,
+      displayText;
+    if (isOpen) {
+      controlText = "Read Less";
+      displayText = text;
+    } else {
+      controlText = "Read More";
+      displayText = text.substring(0, maxLength) + "...";
+    }
+
+    return (
+      <div>
+        {displayText}
+        <Button bsStyle="link" className={styles.expandTextButton} onClick={this.switchState}>({controlText})</Button>
+      </div>
+    );
+
+  }
+}
 
 export class EntitySet extends React.Component {
   static propTypes = {
@@ -32,67 +74,23 @@ export class EntitySet extends React.Component {
     };
   }
 
-  /* Component Logic */
   getUrl(datatype) {
     let entitySet = this.props.entitySet;
     return DataApi.getAllEntitiesOfTypeInSetFileUrl(entitySet.type, entitySet.name, datatype);
   }
 
-  shouldShow = {
-    true: StringConsts.EMPTY,
-    false: styles.hidden
-  };
-
-  changeEditingState = () => {
-    this.setState({
-      editing: !this.state.editing
-    });
-  }
-
-  exitPanel = () => {
-    this.setState({
-      showPanel: false
-    });
-  }
-
-
-  renderPermissionsPanel = (name) => {
-    if (this.props.isOwner) {
-      return (
-        <div className={this.shouldShow[this.state.showPanel]}>
-          <PermissionsPanel entitySetName={name} exitPanel={this.exitPanel} />
-        </div>
-      );
-    }
-    return null;
-  };
-
   render() {
     const { entitySet } = this.props;
-    const { editing } = this.state;
-
-    let isOwner = entitySet.permission.hasPermission(Permission.OWNER);
     let type = entitySet.type;
-
-    let editButton;
-    if (isOwner) {
-      editButton = (
-        <Button onClick={this.changeEditingState}>
-          {(editing) ? 'Stop editing' : 'Edit permissions'}
-        </Button>
-      );
-    }
 
     return (
       <article className={styles.entitySet}>
         <header>
           <h2 className={styles.title}>
-            {name}
-            <small>{type.namespace} {type.title}</small>
+            {entitySet.title}
           </h2>
 
           <div className={styles.controls}>
-            {editButton}
             <DropdownButton title="Actions" id="action-dropdown">
               <MenuItem header>Download</MenuItem>
               <MenuItem href={this.getUrl(FileConsts.CSV)}>CSV</MenuItem>
@@ -107,17 +105,7 @@ export class EntitySet extends React.Component {
             </DropdownButton>
           </div>
         </header>
-
-        {this.renderPermissionsPanel(name)}
-        <PropertyList
-          properties={entitySet.properties}
-          entityTypeName={type.name}
-          entityTypeNamespace={type.namespace}
-          allowEdit={false}
-          entitySetName={entitySet.name}
-          editingPermissions={editing}
-          isOwner={isOwner}
-        />
+        <ExpandableText maxLength={MAX_DESCRIPTION_LENGTH} text={entitySet.description} />
       </article>
     );
   }
