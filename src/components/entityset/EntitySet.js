b4743a32d9eb068f43bd470b57408f9f1c8e5789
Arion Sprague <arion@kryptnostic.com>
2017-01-17 15:28:30 -0800

    Moved ActionDropdown into seperate file

--- src/components/entityset/EntitySet.js
+++ src/components/entityset/EntitySet.js
@@ -1,62 +1,11 @@
 import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
-import { SplitButton, Button, MenuItem } from 'react-bootstrap';
 
-import { DataApi } from 'loom-data';
-
-import FileConsts from '../../utils/Consts/FileConsts';
-import PageConsts from '../../utils/Consts/PageConsts';
+import ActionDropdown from './ActionDropdown';
 import { EntitySetPropType } from './EntitySetStorage';
 import ExpandableText from '../utils/ExpandableText';
 import styles from './entityset.module.css';
 
 const MAX_DESCRIPTION_LENGTH = 300;
-
-class ActionDropdown extends React.Component {
-  static propTypes = {
-    entitySet: EntitySetPropType.isRequired,
-    showDetails: PropTypes.bool
-  };
-
-  render() {
-    const { entitySet } = this.props;
-    const type = entitySet.type;
-
-    let details;
-    if (this.props.showDetails) {
-      details = (
-        <li role="presentation">
-          <Link to={`/entitysets/${this.props.entitySet.id}`}>
-            View Details
-          </Link>
-        </li>
-      );
-    }
-
-    return (
-      <SplitButton pullRight title="Actions" id="action-dropdown">
-        {details}
-        <MenuItem header>Download</MenuItem>
-        <MenuItem href={DataApi.getEntitySetDataFileUrl(entitySet.id, FileConsts.CSV)}>CSV</MenuItem>
-        <MenuItem href={DataApi.getEntitySetDataFileUrl(entitySet.id, FileConsts.JSON)}>JSON</MenuItem>
-        <MenuItem divider/>
-        <li role="presentation">
-          <Link
-            to={{
-              pathname: `/${PageConsts.VISUALIZE}`,
-              query: {
-                name: entitySet.name,
-                typeNamespace: type.namespace,
-                typeName: type.name
-              }
-            }}>
-            Visualize
-          </Link>
-        </li>
-      </SplitButton>
-    );
-  }
-}
 
 /* EntitySet Components */
 export class EntitySetSummary extends React.Component {
