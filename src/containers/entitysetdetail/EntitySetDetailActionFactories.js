e242e5797084d9f5e7a83268a5ca0a9dab59676d
Arion Sprague <arion@kryptnostic.com>
2017-01-18 16:07:05 -0800

    Deleted unused code

--- src/containers/entitysetdetail/EntitySetDetailActionFactories.js
+++ src/containers/entitysetdetail/EntitySetDetailActionFactories.js
@@ -1,23 +1,8 @@
 import * as actionTypes from './EntitySetDetailActionTypes';
-import * as ndataActionFactories from '../ndata/NdataActionFactories';
 
 export function entitySetDetailRequest(id) {
   return {
     type: actionTypes.ENTITY_SET_REQUEST,
     id
-  }
-}
-
-export function entitySetDetailResolve(id) {
-  return {
-    type: actionTypes.ENTITY_SET_RESOLVE,
-    id
-  }
-}
-
-export function entitySetDetailReject(errorMessage) {
-  return {
-    type: actionTypes.ENTITY_SET_REJECT,
-    errorMessage
   }
 }
