2c4d50ab96a308054d10c149b7050f57b3c640a0
Arion Sprague <arion@kryptnostic.com>
2017-01-19 11:36:45 -0800

    Added CreateEntitySet boilerplate code

--- src/containers/datasets/DatasetsComponent.js
+++ src/containers/datasets/DatasetsComponent.js
@@ -1,24 +1,57 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-
-import Page from '../../components/page/Page';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
+import Page from '../../components/page/Page';
+import CreateEntitySet from '../entitysetforms/CreateEntitySet';
 import styles from './datasets.module.css';
 
 class DatasourcesComponent extends React.Component {
   static propTypes = {
+  };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false
+    }
+  }
+
+  onAddDatasource = () => {
+    this.setState({
+      isModalOpen: true
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      isModalOpen: false
+    });
   };
 
   render() {
+    const { isModalOpen } = this.state;
+
     return (
-      <Page>
+      <Page className={styles.datasources}>
         <Page.Header className={styles.pageHeader}>
           <Page.Title className={styles.pageTitle}>Your datasources</Page.Title>
-          <Button bsStyle="primary" className={styles.control}><FontAwesome name="plus-circle" size="lg"/> Datasource</Button>
+          <Button bsStyle="primary" className={styles.control} onClick={this.onAddDatasource}>
+            <FontAwesome name="plus-circle" size="lg"/> Datasource
+          </Button>
         </Page.Header>
+
+        <Modal show={isModalOpen} onHide={this.closeModal} container={this}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a datasource</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <CreateEntitySet onCreate={this.props.onCreateEntityType} entityTypes={[]}/>
+          </Modal.Body>
+        </Modal>
+
         <Page.Body>
 
         </Page.Body>
@@ -35,7 +68,8 @@ function mapStateToProps(state) {
 // TODO: Decide if/how to incorporate bindActionCreators
 function mapDispatchToProps(dispatch) {
   return {
-
+    onCreateEntityType: () => {},
+    loadEntityTypes: () => {}
   };
 }
 
---
+++ src/containers/entitysetforms/CreateEntitySet.js
@@ -0,0 +1,114 @@
+import React, { PropTypes } from 'react';
+import { FormControl, FormGroup, ControlLabel, Button } from 'react-bootstrap';
+import Select from 'react-select';
+import classnames from 'classnames';
+import { connect } from 'react-redux';
+
+import AsyncContent, { AsyncStatePropType } from '../../components/asynccontent/AsyncContent';
+import { EntityTypePropType } from '../../components/entityset/EntitySetStorage';
+
+class CreateEntitySet extends React.Component {
+  static propTypes = {
+    onCreate: PropTypes.func.isRequired,
+    loadEntityTypes: PropTypes.func.isRequired,
+    entityTypes: PropTypes.arrayOf(EntityTypePropType).isRequired,
+    className: PropTypes.string
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: '',
+      entityTypeId: null
+    }
+  }
+
+  componentDidMount() {
+    this.props.loadEntityTypes();
+  }
+
+  onTitleChange = (event) => {
+    this.setState({
+      title: event.target.value
+    });
+  };
+
+  onDescriptionChange = (event) => {
+    this.setState({
+      description: event.target.value
+    });
+  };
+
+  onEntityTypeChange = (option) => {
+    this.setState({
+      entitySetTypeId: option.value
+    });
+  };
+
+  onSubmit = () => {
+    const { title, description, entitySetTypeId} = this.state;
+    const createParams = {};
+
+    if (title) {
+      createParams.title = title;
+    }
+    if (description) {
+      createParams.description = description;
+    }
+    if (entitySetTypeId) {
+      createParams.entitySetTypeId = entitySetTypeId.value;
+    }
+    this.props.onCreate(createParams);
+  };
+
+  getEntityTypeOptions() {
+    return this.props.entityTypes.map(entityType => {
+      return {
+        value: entityType.id,
+        label: entityType.title
+      };
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} className={classnames(this.props.className)}>
+        <FormGroup>
+          <ControlLabel>Title</ControlLabel>
+          <FormControl type="text" onChange={this.onTitleChange}/>
+        </FormGroup>
+
+        <FormGroup>
+          <ControlLabel>Description</ControlLabel>
+          <FormControl componentClass="textarea" onChange={this.onDescriptionChange}/>
+        </FormGroup>
+
+        <FormGroup>
+          <ControlLabel>Entity type</ControlLabel>
+          <Select
+            value={this.state.entitySetTypeId}
+            options={this.getEntityTypeOptions()}
+            onChange={this.onEntityTypeChange}
+          />
+        </FormGroup>
+        <Button type="submit" bsStyle="primary">Create</Button>
+      </form>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+  };
+}
+
+// TODO: Decide if/how to incorporate bindActionCreators
+function mapDispatchToProps(dispatch) {
+  return {
+    onCreate: () => {},
+    loadEntityTypes: () => {}
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateEntitySet);
