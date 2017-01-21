3450144dff5ab6014219d6f60ebcf88edfe0d972
Katherine <katherine@kryptnostic.com>
2017-01-20 18:02:45 -0800

    oops

--- src/containers/catalog/CatalogComponent.js
+++ src/containers/catalog/CatalogComponent.js
@@ -10,28 +10,6 @@ import SecurableObjectSearch, { FilterParamsPropType } from '../securableobject/
 import { catalogSearchRequest } from './CatalogActionFactories';
 import AsyncContent, { AsyncStatePropType } from '../../components/asynccontent/AsyncContent';
 
-// TODO: Replace with Async calls
-const ENTITY_SET_TYPE_OPTIONS = [{
-  value: 'c65a53e7-5647-4f93-aeac-9ce8a1298fc6',
-  label: 'Employee'
-}];
-
-const PROPERTY_TYPE_OPTIONS = [{
-  value: '033fef2a-8f34-4bcd-b1ad-e123c462561d',
-  label: 'Employee ID'
-},{
-  value: 'e76bc4e4-cf6a-43f4-a338-d241ded39093',
-  label: 'Employee ID'
-},{
-  value: '9727370f-8506-402c-8f35-2da4dbbb3c06',
-  label: 'Employee Title'
-},{
-  value: 'eb15c62d-fe91-4231-abb7-1228759cae43',
-  label: 'Employee Salary'
-},{
-  value: '0bae0920-2b89-4da0-8af9-8079a52d9e98',
-  label: 'Employee Department'
-}];
 
 class CatalogComponent extends React.Component {
   static propTypes = {
