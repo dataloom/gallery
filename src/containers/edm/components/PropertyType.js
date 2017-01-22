2cf7d5e56bff2ac1b87488cf044189e103e953d7
Arion Sprague <arion@kryptnostic.com>
2017-01-21 19:19:22 -0800

    Added requestAccess permissions

--- src/containers/edm/components/PropertyType.js
+++ src/containers/edm/components/PropertyType.js
@@ -44,7 +44,7 @@ class PropertyType extends React.Component {
     if (editing.permissions) {
       // TODO: Support more than just read
       // TODO: Enforce entitySetId on edit
-      content = (<Checkbox checked={canRead}/>);
+      content = (<input type="checkbox" defaultChecked={canRead}/>);
     } else if (!canRead) {
       content = (<FontAwesome name="lock"/>);
     }
--- src/containers/permissions/components/RequestPermissionsModal.js
+++ src/containers/permissions/components/RequestPermissionsModal.js
@@ -6,6 +6,7 @@ import * as actionFactory from '../PermissionsActionFactory';
 import PropertyTypeList from '../../edm/components/PropertyTypeList';
 import { getEdmObjectSilent, createEntitySetReference } from '../../edm/EdmStorage';
 import { EntitySetPropType } from '../../edm/EdmModel';
+import styles from './requestPermissionsModal.module.css';
 
 const PROPERTY_TYPE_EDITING = {
   permissions: true
@@ -39,7 +40,8 @@ class RequestPermissions extends React.Component {
           <Modal.Title>{ title }</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form onSubmit={this.onSubmit}>
+          <form onSubmit={this.onSubmit} className={styles.rqm}>
+            <h2 className={styles.subtitle}>Select properties you want access to:</h2>
             <PropertyTypeList entitySetId={entitySetId} propertyTypeIds={propertyTypeIds} editing={PROPERTY_TYPE_EDITING}/>
             <Button type="submit" bsStyle="primary">Search</Button>
           </form>
