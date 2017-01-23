e7720b105e883f2f1cd3e789495a224095bd54d3
Arion Sprague <arion@kryptnostic.com>
2017-01-22 17:51:25 -0800

    wired permissions requests

--- src/containers/permissions/PermissionsEpic.js
+++ src/containers/permissions/PermissionsEpic.js
@@ -10,6 +10,10 @@ import {
   Observable
 } from 'rxjs';
 
+import {
+  combineEpics
+} from 'redux-observable'
+
 import * as PermissionsActionTypes from './PermissionsActionTypes';
 import * as PermissionsActionFactory from './PermissionsActionFactory';
 
@@ -17,8 +21,31 @@ import {
   deserializeAuthorization
 } from './PermissionsStorage';
 
+import type {
+  AccessCheck,
+  PermissionsRequest
+} from './PermissionsStorage'
+
+import {
+  permissionsRequest
+} from '../Api';
+
+function requestPermissions(requests :PermissionsRequest[]) :Observable<Action> {
+  return Observable
+    .of(requests)
+    // .from(permissionsRequest(requests))
+    .map(console.log);
+}
+
+function requestPermissionsEpic(action$ :Observable<Action>) :Observable<Action> {
+  return action$
+    .ofType(PermissionsActionTypes.REQUEST_PERMISSIONS_REQUEST)
+    .pluck('requests')
+    .mergeMap(requestPermissions);
+}
+
 // TODO: Save property types
-function authorizationCheck(accessChecks :Object[]) :Observable<Action> {
+function authorizationCheck(accessChecks :AccessCheck[]) :Observable<Action> {
 
   return Observable
     .from(AuthorizationApi.checkAuthorizations(accessChecks))
@@ -34,10 +61,12 @@ function authorizationCheck(accessChecks :Object[]) :Observable<Action> {
 }
 
 // TODO: Cancellation and Error handling
-export default function authorizationCheckEpic(action$ :Observable<Action>) :Observable<Action> {
+function authorizationCheckEpic(action$ :Observable<Action>) :Observable<Action> {
 
   return action$
     .ofType(PermissionsActionTypes.CHECK_AUTHORIZATION_REQUEST)
     .pluck('accessChecks')
     .mergeMap(authorizationCheck);
 }
+
+export default combineEpics(authorizationCheckEpic, requestPermissionsEpic);
