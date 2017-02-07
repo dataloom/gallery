d324c663f96d6b7c38a2f97ae5435cd923e824d6
Hristo Oskov <hristo@kryptnostic.com>
2017-02-06 17:20:11 -0800

    row, column flex containers using styled-components

---
+++ src/components/flex/StyledFlexContainer.js
@@ -0,0 +1,14 @@
+/*
+ * @flow
+ */
+
+import React from 'react';
+
+import styled from 'styled-components';
+
+const StyledFlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export default StyledFlexContainer;
---
+++ src/components/flex/StyledStackedFlexContainer.js
@@ -0,0 +1,14 @@
+/*
+ * @flow
+ */
+
+import React from 'react';
+
+import styled from 'styled-components';
+
+const StyledStackedFlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default StyledStackedFlexContainer;
