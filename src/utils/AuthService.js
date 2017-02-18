e8d3212b0605ee160d76fcb9cc1db5ebfc694403
Corwin Crownover <corwin@thedataloom.com>
2017-02-17 17:57:00 -0800

    Use PageConsts for redirect

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -7,6 +7,7 @@ import * as Cookies from 'js-cookie';
 
 import { isTokenExpired } from './jwtHelper';
 import img from '../images/kryptnostic-logo-big.png';
+import PageConsts from './Consts/PageConsts.js';
 
 export default class AuthService extends EventEmitter {
   constructor(clientId, domain, isLocal) {
@@ -51,7 +52,7 @@ export default class AuthService extends EventEmitter {
 
     //Bugfix for redirect race conditions in IE & Safari
     //As soon as authentication is complete -> redirect to home
-    hashHistory.push('/home');
+    hashHistory.push('/' + PageConsts.HOME);
   }
 
   authorizationError(error) {
