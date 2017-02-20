28b8c7b71dddbbe381699b950a3e80f79f955847
Corwin Crownover <corwin@thedataloom.com>
2017-02-20 11:12:02 -0800

    Remove hashHistory import

--- src/views/Main/routes.js
+++ src/views/Main/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hashHistory, Route, IndexRedirect } from 'react-router';
+import { Route, IndexRedirect } from 'react-router';
 import Loom from 'loom-data';
 import AuthService from '../../utils/AuthService';
 import Container from './Container';
