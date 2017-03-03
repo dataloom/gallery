279690efd87a828051acaa55abcc5572e2273685
Corwin Crownover <corwin@thedataloom.com>
2017-03-02 18:04:05 -0800

    Clean up code

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -29,7 +29,6 @@ class Container extends React.Component {
   }
 
   componentDidMount() {
-    console.log('state name:', this.state.name);
     if (this.props.route.auth.loggedIn()) {
       this.props.loadPropertyTypes();
       this.props.loadEntityTypes();
