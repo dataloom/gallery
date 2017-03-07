aa9bafeff1e45262762d9da03b398fcaccfd9ce9
Arion Sprague <arion@kryptnostic.com>
2017-03-07 15:01:23 -0800

    Added initial webpack integration to karma

--- config/karma/karma.conf.js
+++ config/karma/karma.conf.js
@@ -1,3 +1,54 @@
+/* Webpack Config */
+const APP_PATHS = require('../app.paths.js');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const BABEL_LOADER = {
+  loader: 'babel-loader',
+  test: /\.(js|jsx)$/,
+  include: [
+    APP_PATHS.ABS.SOURCE,
+    APP_PATHS.ABS.TEST
+  ]
+};
+
+const CSS_LOADER = {
+  loader: ExtractTextPlugin.extract(
+    'css-loader?modules&localIdentName=[local]--[hash:base64:5]&importLoader=1!postcss-loader?config=config/postcss/postcss.config.js'
+  ),
+  test: /\.css$/,
+  exclude: /src\/core\/styles\/global\/.*\.css/
+};
+
+const CSS_LOADER_GLOBALS = {
+  loader: ExtractTextPlugin.extract(
+    'css-loader'
+  ),
+  test: /\.css$/,
+  include: /src\/core\/styles\/global\/.*\.css/
+};
+
+const JSON_LOADER = {
+  loader: 'json-loader',
+  test: /\.json$/
+};
+
+const MEDIA_FILE_LOADER = {
+  loader: 'file-loader',
+  test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+  query: {
+    name: `${APP_PATHS.REL.STATIC_MEDIA}/[name].[hash:8].[ext]`
+  }
+};
+
+const MEDIA_URL_LOADER = {
+  loader: 'url-loader',
+  test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
+  query: {
+    limit: 10000,
+    name: `${APP_PATHS.REL.STATIC_MEDIA}/[name].[hash:8].[ext]`
+  }
+};
+
 // Karma configuration
 
 const TEST_GLOB_PATH = '../../src/**/*.test.js';
@@ -23,9 +74,20 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      TEST_GLOB_PATH: ['webpack']
+      [TEST_GLOB_PATH]: ['webpack']
     },
-
+    webpack: {
+      module: {
+        rules: [
+          BABEL_LOADER,
+          CSS_LOADER,
+          CSS_LOADER_GLOBALS,
+          JSON_LOADER,
+          MEDIA_FILE_LOADER,
+          MEDIA_URL_LOADER
+        ]
+      },
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
