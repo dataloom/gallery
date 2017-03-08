278605daa2b5631027c5e9dc652fd1a9bd73128a
Arion Sprague <arion@kryptnostic.com>
2017-03-08 15:52:27 -0800

    Babelified webpack.config.test.js

--- config/webpack/webpack.config.test.js
+++ config/webpack/webpack.config.test.js
@@ -1,21 +1,32 @@
-const loaders = require('./loaders');
-const plugins = require('./plugins');
+import {
+  BABEL_LOADER,
+  CSS_LOADER,
+  CSS_LOADER_GLOBALS,
+  JSON_LOADER,
+  MEDIA_FILE_LOADER,
+  MEDIA_URL_LOADER
+} from './loaders';
+
+import {
+  DEFINE_PLUGIN,
+  EXTRACT_TEXT_PLUGIN
+} from './plugins';
 
 
-module.exports = {
+export default {
   module: {
     rules: [
-      loaders.BABEL_LOADER,
-      loaders.CSS_LOADER,
-      loaders.CSS_LOADER_GLOBALS,
-      loaders.JSON_LOADER,
-      loaders.MEDIA_FILE_LOADER,
-      loaders.MEDIA_URL_LOADER
+      BABEL_LOADER,
+      CSS_LOADER,
+      CSS_LOADER_GLOBALS,
+      JSON_LOADER,
+      MEDIA_FILE_LOADER,
+      MEDIA_URL_LOADER
     ]
   },
   plugins: [
-    plugins.DEFINE_PLUGIN,
-    plugins.EXTRACT_TEXT_PLUGIN
+    DEFINE_PLUGIN,
+    EXTRACT_TEXT_PLUGIN
   ],
   devtool: 'inline-source-map'
 };
