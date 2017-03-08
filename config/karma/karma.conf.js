1af8fd5d307562dfd5e69c9697e41f54b466b8a1
Arion Sprague <arion@kryptnostic.com>
2017-03-07 16:01:41 -0800

    Added babel-polyfill to tests

--- config/karma/karma.conf.js
+++ config/karma/karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      require.resolve('babel-polyfill'),
       TEST_GLOB_PATH
     ],
 
@@ -23,6 +24,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      [require.resolve('babel-polyfill')]: ['webpack'],
       [TEST_GLOB_PATH]: ['webpack']
     },
     webpack: require('../webpack/webpack.config.test'),
