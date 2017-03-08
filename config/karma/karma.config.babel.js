7f13ce4e4af9a725d2e45b3f9f103083f4a9d7eb
Hristo Oskov <hristo@kryptnostic.com>
2017-03-08 15:09:21 -0800

    adding comments to karma config

--- config/karma/karma.config.babel.js
+++ config/karma/karma.config.babel.js
@@ -6,6 +6,7 @@ const CHAI_CONFIG_PATH = 'config/chai/*.js';
 const ENV = process.env.TEST;
 
 export default function(config) {
+
   let autoWatch = false;
   let browsers = ['PhantomJS'];
 
@@ -15,57 +16,88 @@ export default function(config) {
   }
 
   config.set({
+
+    /*
+     * enables or disables watching files so to execute the tests whenever a file changes
+     */
     autoWatch,
+
+    /*
+     * continuous integration mode
+     * if true, Karma will start and capture all configured browsers, run the tests, and then exit with an exit code of
+     * 0 or 1; 0 if all tests passed, 1 if any tests failed
+     */
     singleRun: !autoWatch,
+
+    /*
+     * a list of browsers to launch and capture
+     *
+     * http://karma-runner.github.io/1.0/config/browsers.html
+     * https://npmjs.org/browse/keyword/karma-launcher
+     */
     browsers,
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
+    // root path that will be used to resolve all relative paths defined in "files" and "exclude"
     basePath: '../..',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    /*
+     * a list of test frameworks to use
+     *
+     * https://npmjs.org/browse/keyword/karma-adapter
+     */
     frameworks: ['mocha', 'chai'],
 
-
-    // list of files / patterns to load in the browser
+    /*
+     * a list of files to load in the browser
+     *
+     * http://karma-runner.github.io/1.0/config/files.html
+     */
     files: [
       require.resolve('babel-polyfill'),
       CHAI_CONFIG_PATH,
       TESTS_PATH
     ],
 
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    /*
+     * the keys in the "preprocessors" config filter the matching files specified in the "files" config for processing
+     * before serving them to the browser
+     *
+     * http://karma-runner.github.io/1.0/config/preprocessors.html
+     * https://npmjs.org/browse/keyword/karma-preprocessor
+     */
     preprocessors: {
       [require.resolve('babel-polyfill')]: ['webpack'],
       [CHAI_CONFIG_PATH]: ['webpack'],
       [TESTS_PATH]: ['webpack', 'sourcemap']
     },
+
+    /*
+     * https://github.com/webpack/karma-webpack
+     */
     webpack: WEBPACK_CONFIG,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    /*
+     * https://webpack.github.io/docs/webpack-dev-middleware.html
+     */
+    webpackMiddleware: {
+      noInfo: true
+    },
+
+    /*
+     * a list of reporters to use for test results
+     *
+     * https://npmjs.org/browse/keyword/karma-reporter
+     */
     reporters: ['progress'],
 
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
+    /*
+     * possible values:
+     *   config.LOG_DISABLE
+     *   config.LOG_ERROR
+     *   config.LOG_WARN
+     *   config.LOG_INFO
+     *   config.LOG_DEBUG
+     */
+    logLevel: config.LOG_INFO
   });
-};
+}
