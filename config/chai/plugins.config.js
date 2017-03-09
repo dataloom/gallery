f063a755909ca2e0c87353af84397a40df886682
Arion Sprague <arion@kryptnostic.com>
2017-03-08 16:01:00 -0800

    Fixed comment indentation

--- config/chai/plugins.config.js
+++ config/chai/plugins.config.js
@@ -1,10 +1,10 @@
 /*
-** Hack to get chai plugins working. How it works:
-* karma-chai is regestered as 'chai' in karma.config.js under 'frameworks'
-* karma-chai is run before any tests, and adds expect, should, and assert to the global window object
-* Anything in test/ is run before src/ because of the files order
-* This file adds chaiImmutable to the global `chai` instance, which adds the plugin to the global `expect`
-* Importing chai (or expect) either here or in tests will prevent the plugins from being applied.
+ * Hack to get chai plugins working. How it works:
+ * karma-chai is regestered as 'chai' in karma.config.js under 'frameworks'
+ * karma-chai is run before any tests, and adds expect, should, and assert to the global window object
+ * Anything in test/ is run before src/ because of the files order
+ * This file adds chaiImmutable to the global `chai` instance, which adds the plugin to the global `expect`
+ * Importing chai (or expect) either here or in tests will prevent the plugins from being applied.
  */
 
 import chaiImmutable from 'chai-immutable';
