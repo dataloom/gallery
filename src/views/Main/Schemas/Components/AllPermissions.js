639596dc6ec44bd59977aa4f40ce7bc2d8ab0b11
Corwin Crownover <corwin@thedataloom.com>
2017-03-10 16:32:56 -0800

    Set up AllPermissions logic to generate tables for entity and each property separately

--- src/views/Main/Schemas/Components/AllPermissions.js
+++ src/views/Main/Schemas/Components/AllPermissions.js
@@ -32,205 +32,76 @@ const permissionOptions = {
   Owner: 'Owner'
 };
 
-const U_HEADERS = ['Users', 'Roles', 'Entity Permissions', 'Property A permissions', 'Property B Permissions'];
+const U_HEADERS = ['Users', 'Roles', 'Permissions'];
 
 const USERS = [{
   id: 0,
   email: 'corwin@thedataloom.com',
   role: '(all)',
-  entityPermissions: 'Read, Write, Discover, Link',
-  propertyAPermissions: 'Read, Write',
-  propertyBPermissions: '-',
+  permissions: 'Read, Write, Discover, Link',
   expand: [{
     role: 'admin',
-    entityPermissions: 'Read, Write, Discover, Link',
-    propertyAPermissions: 'Read, Write',
-    propertyBPermissions: '-'
+    permissions: 'Read, Write, Discover, Link'
   }, {
     role: 'xyz',
-    entityPermissions: 'Read, Write',
-    propertyAPermissions: 'Read, Write',
-    propertyBPermissions: 'Read, Write'
+    permissions: 'Read, Write'
   }]
 }, {
   id: 1,
   email: 'matthew@thedataloom.com',
   role: '(all)',
-  entityPermissions: ['read', 'write'],
-  propertyAPermissions: ['read'],
-  propertyBPermissions: ['read'],
+  permissions: ['read', 'write'],
   expand: [{
     role: 'role',
-    entityPermissions: ['read', 'write', 'discover', 'link'],
-    propertyAPermissions: ['read', 'write'],
-    propertyBPermissions: []
+    permissions: ['read', 'write', 'discover', 'link']
   }]
 }, {
   id: 2,
   email: 'katherine@thedataloom.com',
   role: '(all)',
-  entityPermissions: ['read'],
-  propertyAPermissions: ['read'],
-  propertyBPermissions: ['read'],
+  permissions: ['read'],
   expand: [{
     role: 'role',
-    entityPermissions: ['read', 'write', 'discover', 'link'],
-    propertyAPermissions: ['read', 'write'],
-    propertyBPermissions: []
+    permissions: ['read', 'write', 'discover', 'link']
   }]
 }];
 
 const R_HEADERS = ['Roles', 'Permissions'];
 
 class AllPermissions extends React.Component {
-  static propTypes = {
-    entitySetId: PropTypes.string,
-    propertyTypeId: PropTypes.string,
-  }
-
   constructor(props) {
     super(props);
 
     this.state = {
-      view: views.GLOBAL,
-      updateSuccess: false,
-      updateError: false,
-      globalValue: [],
-      roleAcls: { Discover: [], Link: [], Read: [], Write: [] },
-      userAcls: { Discover: [], Link: [], Read: [], Write: [], Owner: [] },
-      rolesView: accessOptions.Write,
-      emailsView: accessOptions.Write,
-      newRoleValue: '',
-      newEmailValue: '',
-      allUsersById: {},
-      allRolesList: new Set(),
-      loadUsersError: false,
-      entitySetId: JSON.parse(localStorage.entitySet).id,
-      propertyTypeIds: JSON.parse(localStorage.propertyTypeIds)
+      entityUserPermissions: []
     };
 
-    this.getUserPermissions = this.getUserPermissions.bind(this);
-  }
-
-  componentDidMount() {
-    // this.getUserPermissions();
-    // this.getRolePermissions();
-
-    this.loadAcls(false);
+    this.getEntityUserPermissions = this.getEntityUserPermissions.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
     console.log('PROPS!:', nextProps);
 
     if (this.props.allUsersById === undefined && nextProps.allUsersById !== undefined) {
-      console.log('FINALLY! REDUX PROPS!:', nextProps);
+      console.log('WE GOT ALL THE USERS!');
+      this.getEntityUserPermissions();
+      this.getRolePermissions();
+    }
+
+    if (this.props.properties === undefined && nextProps.properties !== undefined) {
+      const { properties } = nextProps;
+      console.log('WE GOT THE PROPERTIES!!:', properties);
+      // Object.keys(properties).forEach((property) => {
+      //   this.getPropertyUserPermissions(property);
+      //   this.getPropertyRolePermissions(property);
+      // });
     }
   }
 
-  loadAllUsersAndRoles = () => {
-    let allUsersById = {};
-    const allRolesList = new Set();
-    const myId = JSON.parse(localStorage.profile).user_id;
-    PrincipalsApi.getAllUsers()
-    .then((users) => {
-      allUsersById = users;
-      Object.keys(users).forEach((userId) => {
-        users[userId].roles.forEach((role) => {
-          if (role !== AUTHENTICATED_USER) allRolesList.add(role);
-        });
-      });
-      allUsersById[myId] = null;
-
-      return new Promise((resolve) => {
-        this.setState(
-          {
-            allUsersById,
-            allRolesList,
-            loadUsersError: false
-          },
-          () => {
-            resolve(this.state.allUsersById);
-          }
-        );
-      });
-    })
-    ///////////TODO: REMOVE ONCE THIS LOGIC IS ON ESDC: CAN CALL ONCE ALLPERMISSIONS IS MOUNTED AND PROPS PASSED IN
-    .then(() => {
-      this.getUserPermissions();
-      this.getRolePermissions();
-    })
-    .catch(() => {
-      this.setState({ loadUsersError: true });
-    });
-  }
-
-  getPermission = (permissions) => {
-    const newPermissions = [];
-    if (permissions.includes(permissionOptions.Owner.toUpperCase())) return [permissionOptions.Owner];
-    if (permissions.includes(permissionOptions.Write.toUpperCase())) newPermissions.push(permissionOptions.Write);
-    if (permissions.includes(permissionOptions.Read.toUpperCase())) newPermissions.push(permissionOptions.Read);
-    if (permissions.includes(permissionOptions.Link.toUpperCase())) newPermissions.push(permissionOptions.Link);
-    if (permissions.includes(permissionOptions.Discover.toUpperCase())) newPermissions.push(permissionOptions.Discover);
-    return newPermissions;
-  }
-
-  updateStateAcls = (aces, updateSuccess) => {
-    let globalValue = [];
-    const roleAcls = { Discover: [], Link: [], Read: [], Write: [] };
-    const userAcls = { Discover: [], Link: [], Read: [], Write: [], Owner: [] };
-    aces.forEach((ace) => {
-      if (ace.permissions.length > 0) {
-        if (ace.principal.type === ROLE) {
-          if (ace.principal.id === AUTHENTICATED_USER) {
-            globalValue = this.getPermission(ace.permissions);
-          }
-          else {
-            this.getPermission(ace.permissions).forEach((permission) => {
-              roleAcls[permission].push(ace.principal.id);
-            });
-          }
-        }
-        else {
-          this.getPermission(ace.permissions).forEach((permission) => {
-            userAcls[permission].push(ace.principal.id);
-          });
-        }
-      }
-    });
-    this.setState({
-      globalValue,
-      roleAcls,
-      userAcls,
-      updateSuccess,
-      newRoleValue: '',
-      newEmailValue: '',
-      updateError: false
-    });
-  }
-
-  loadAcls = (updateSuccess) => {
-    const { entitySetId } = this.state;
-    const { propertyTypeId } = this.props;
-    const aclKey = [entitySetId];
-    if (propertyTypeId) aclKey.push(propertyTypeId);
-    this.loadAllUsersAndRoles();
-
-    PermissionsApi.getAcl(aclKey)
-    .then((acls) => {
-      this.updateStateAcls(acls.aces, updateSuccess);
-    })
-    .catch(() => {
-      this.setState({ updateError: true });
-    });
-  }
-
-
-///////////////// LOGIC FOR ALLPERMISSIONS ////////////////////////
-  getUserPermissions = () => {
-    // const { allUsersById, userAcls, roleAcls } = this.props;
-    const { userAcls, roleAcls } = this.state;
+  getEntityUserPermissions = () => {
+    const { userAcls, roleAcls } = this.props;
+    console.log('USERACLS, ROLEACLS:', userAcls, roleAcls);
     const { allUsersById } = this.props;
-    console.log('ALLUSERSBYID:', allUsersById);
     const userPermissions = [];
 
     // For each user, add their permissions
@@ -240,12 +111,9 @@ class AllPermissions extends React.Component {
           id: userId,
           email: allUsersById[userId].email,
           roles: [],
-          entityPermissions: [],
-          propertyPermissions: [],
-          expand: []
+          permissions: [],
+          expand: [] // set of {role, permissions}
         };
-
-        // TODO: FOR EACH PROPERTY, ADD PROPERTY KEY + VALUES (PERMISSIONS);
 
         // Add individual permissions
         Object.keys(userAcls).forEach((permissionKey) => {
@@ -269,10 +137,11 @@ class AllPermissions extends React.Component {
         userPermissions.push(user);
       }
     });
-    this.setUserPermissions(userPermissions);
+    console.log('USER PERMISSIONS: ', userPermissions);
+    this.setEntityUserPermissions(userPermissions);
   }
 
-  setUserPermissions = (permissions) => {
+  setEntityUserPermissions = (permissions) => {
     const formattedPermissions = permissions.slice();
 
     // Format permissions for table
@@ -287,10 +156,10 @@ class AllPermissions extends React.Component {
 
     });
 
-    this.setState({ userPermissions: formattedPermissions });
+    this.setState({ entityUserPermissions: formattedPermissions }, () => {console.log('ENTITY USER PERMISSIONS:', this.state.entityUserPermissions)});
   }
 
-  getRolePermissions = () => {
+  getRolePermissions = (set) => {
     // const { roleAcls } = this.props;
     const { roleAcls } = this.state;
     const rolePermissions = {};
@@ -329,9 +198,8 @@ class AllPermissions extends React.Component {
           <Page.Title>All Permissions</Page.Title>
         </Page.Header>
         <Page.Body>
-          <h3>Individual Permissions</h3>
+          <h3>Entity Permissions</h3>
           <UserPermissionsTable users={USERS} headers={U_HEADERS} />
-          <h3>Role Permissions</h3>
           <RolePermissionsTable roles={this.state.rolePermissions} headers={R_HEADERS} />
         </Page.Body>
       </Page>
@@ -341,14 +209,16 @@ class AllPermissions extends React.Component {
 
 function mapStateToProps(state) {
   const entitySetDetail = state.get('entitySetDetail');
-  console.log('ENTITYSETDETAIL props:', entitySetDetail.toJS());
   return {
-    allUsersById: entitySetDetail.get('allUsersById').toJS()
+    allUsersById: entitySetDetail.get('allUsersById').toJS(),
+    properties: entitySetDetail.get('properties').toJS()
   };
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
+  return {
 
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllPermissions);
