8c907117254e3a5722abbebd25f9205e73d604be
Katherine <katherine@kryptnostic.com>
2017-03-22 10:49:22 -0700

    flow, rename things

--- src/containers/visualizations/VisualizationReducer.js
+++ src/containers/visualizations/VisualizationReducer.js
@@ -10,15 +10,14 @@ import * as VisualizationActionTypes from './VisualizationActionTypes';
 const INITIAL_STATE :Map<*, *> = Immutable.fromJS({
   visualizableEntitySets: Immutable.List(),
   entitySet: Immutable.Map(),
-  propertyTypes: Immutable.List(),
   numberProps: Immutable.List(),
   geoProps: Immutable.List(),
   dateProps: Immutable.List(),
   results: Immutable.List(),
   chartOptions: Immutable.List(),
   errorMessage: '',
-  isLoadingDataStatus: ASYNC_STATUS.PENDING,
-  isLoadingVisualizableEntitySetsStatus: ASYNC_STATUS.PENDING
+  loadingDataStatus: ASYNC_STATUS.PENDING,
+  loadingVisualizableEntitySetsStatus: ASYNC_STATUS.PENDING
 });
 
 export default function visualizationsReducer(state :Immutable.Map = INITIAL_STATE, action :Object) :Immutable.Map {
@@ -26,99 +25,87 @@ export default function visualizationsReducer(state :Immutable.Map = INITIAL_STA
   switch (action.type) {
 
     case VisualizationActionTypes.LOAD_ENTITY_SET_REQUEST:
-      return INITIAL_STATE.set('isLoadingDataStatus', ASYNC_STATUS.LOADING);
+      return INITIAL_STATE.set('loadingDataStatus', ASYNC_STATUS.LOADING);
 
     case VisualizationActionTypes.LOAD_ENTITY_SET_SUCCESS:
-      return state.set('entitySet', action.entitySet);
+      return state.set('entitySet', Immutable.fromJS(action.entitySet));
 
     case VisualizationActionTypes.LOAD_ENTITY_SET_FAILURE:
       return state
         .set('errorMessage', action.errorMessage)
         .set('entitySet', Immutable.Map())
-        .set('isLoadingDataStatus', ASYNC_STATUS.ERROR);
+        .set('loadingDataStatus', ASYNC_STATUS.ERROR);
 
     case VisualizationActionTypes.LOAD_ENTITY_TYPE_REQUEST:
       return state;
 
     case VisualizationActionTypes.LOAD_ENTITY_TYPE_SUCCESS:
-      return state.set('propertyTypes', Immutable.Map());
+      return state;
 
     case VisualizationActionTypes.LOAD_ENTITY_TYPE_FAILURE:
       return state
         .set('errorMessage', action.errorMessage)
-        .set('isLoadingDataStatus', ASYNC_STATUS.ERROR);
+        .set('loadingDataStatus', ASYNC_STATUS.ERROR);
 
     case VisualizationActionTypes.LOAD_PROPERTY_TYPES_REQUEST:
       return state;
 
     case VisualizationActionTypes.LOAD_PROPERTY_TYPES_SUCCESS: {
-      const isLoadingDataStatus = (action.chartOptions.length > 1) ? ASYNC_STATUS.LOADING : ASYNC_STATUS.SUCCESS;
+      const loadingDataStatus = (action.chartOptions.length > 1) ? ASYNC_STATUS.LOADING : ASYNC_STATUS.SUCCESS;
       return state
-        .set('numberProps', action.numberProps)
-        .set('dateProps', action.dateProps)
-        .set('geoProps', action.geoProps)
-        .set('chartOptions', action.chartOptions)
-        .set('isLoadingDataStatus', isLoadingDataStatus);
+        .set('numberProps', Immutable.fromJS(action.numberProps))
+        .set('dateProps', Immutable.fromJS(action.dateProps))
+        .set('geoProps', Immutable.fromJS(action.geoProps))
+        .set('chartOptions', Immutable.fromJS(action.chartOptions))
+        .set('loadingDataStatus', loadingDataStatus);
     }
 
     case VisualizationActionTypes.LOAD_PROPERTY_TYPES_FAILURE:
       return state
         .set('errorMessage', action.errorMessage)
-        .set('isLoadingDataStatus', ASYNC_STATUS.ERROR);
-
-    case VisualizationActionTypes.CHECK_AUTHORIZATIONS_REQUEST:
-      return state;
-
-    case VisualizationActionTypes.CHECK_AUTHORIZATIONS_SUCCESS:
-      return state;
+        .set('loadingDataStatus', ASYNC_STATUS.ERROR);
 
     case VisualizationActionTypes.CHECK_AUTHORIZATIONS_FAILURE:
       return state
         .set('errorMessage', action.errorMessage)
-        .set('isLoadingDataStatus', ASYNC_STATUS.ERROR);
-
-    case VisualizationActionTypes.GET_DATA_REQUEST:
-      return state;
+        .set('loadingDataStatus', ASYNC_STATUS.ERROR);
 
     case VisualizationActionTypes.GET_DATA_SUCCESS:
       return state
-        .set('results', action.results)
-        .set('isLoadingDataStatus', ASYNC_STATUS.SUCCESS);
+        .set('results', Immutable.fromJS(action.results))
+        .set('loadingDataStatus', ASYNC_STATUS.SUCCESS);
 
     case VisualizationActionTypes.GET_DATA_FAILURE:
       return state
         .set('errorMessage', action.errorMessage)
         .set('results', Immutable.List())
-        .set('isLoadingDataStatus', ASYNC_STATUS.ERROR);
+        .set('loadingDataStatus', ASYNC_STATUS.ERROR);
 
     case VisualizationActionTypes.GET_ALL_ENTITY_SETS_REQUEST:
-      return INITIAL_STATE.set('isLoadingVisualizableEntitySetsStatus', ASYNC_STATUS.LOADING);
-
-    case VisualizationActionTypes.GET_ALL_ENTITY_SETS_SUCCESS:
-      return state;
+      return INITIAL_STATE.set('loadingVisualizableEntitySetsStatus', ASYNC_STATUS.LOADING);
 
     case VisualizationActionTypes.GET_ALL_ENTITY_SETS_FAILURE:
       return state
         .set('errorMessage', action.errorMessage)
-        .set('isLoadingVisualizableEntitySetsStatus', ASYNC_STATUS.ERROR);
+        .set('loadingVisualizableEntitySetsStatus', ASYNC_STATUS.ERROR);
 
     case VisualizationActionTypes.GET_ALL_ENTITY_TYPES_FOR_SETS_FAILURE:
       return state
         .set('errorMessage', action.errorMessage)
-        .set('isLoadingVisualizableEntitySetsStatus', ASYNC_STATUS.ERROR);
+        .set('loadingVisualizableEntitySetsStatus', ASYNC_STATUS.ERROR);
 
     case VisualizationActionTypes.GET_ALL_PROPERTY_TYPES_FOR_SETS_FAILURE:
       return state
         .set('errorMessage', action.errorMessage)
-        .set('isLoadingVisualizableEntitySetsStatus', ASYNC_STATUS.ERROR);
+        .set('loadingVisualizableEntitySetsStatus', ASYNC_STATUS.ERROR);
 
     case VisualizationActionTypes.GET_VISUALIZABLE_ENTITY_SETS_SUCCESS:
       return state
-        .set('visualizableEntitySets', action.visualizableEntitySets)
-        .set('isLoadingVisualizableEntitySetsStatus', ASYNC_STATUS.SUCCESS);
+        .set('visualizableEntitySets', Immutable.fromJS(action.visualizableEntitySets))
+        .set('loadingVisualizableEntitySetsStatus', ASYNC_STATUS.SUCCESS);
 
     case VisualizationActionTypes.GET_VISUALIZABLE_ENTITY_SETS_FAILURE:
-      return state.set('isLoadingVisualizableEntitySetsStatus', ASYNC_STATUS.ERROR);
+      return state.set('loadingVisualizableEntitySetsStatus', ASYNC_STATUS.ERROR);
 
     default:
       return state;
