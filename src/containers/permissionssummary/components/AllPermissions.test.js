be0a05d17be41ed31a8fc3724a442cf039dd7be2
Corwin Crownover <corwin@thedataloom.com>
2017-04-05 16:51:55 -0700

    Implement tests for mapDispatchToProps

---
+++ src/containers/permissionssummary/components/AllPermissions.test.js
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+import '../../../../config/chai/chai.config';
+
+import * as PSActionFactory from '../PermissionsSummaryActionFactory';
+import * as ESDCActionFactory from '../../entitysetdetail/EntitySetDetailActionFactories';
+import * as PermissionsActionFactory from '../../permissions/PermissionsActionFactory';
+import * as EDMActionFactory from '../../edm/EdmActionFactories';
+import { mapDispatchToProps } from './AllPermissions';
+
+describe('PermissionsSummary Component', function() {
+  // Consts
+
+  describe('mapDispatchToProps', function() {
+    let dispatch;
+    let mappedProps;
+
+    beforeEach(function() {
+      dispatch = sinon.spy();
+      mappedProps = mapDispatchToProps(dispatch);
+    });
+
+    describe('loadEntitySet', function() {
+      const id = 'id';
+
+      it('should dispatch entitySetDetailRequest', function() {
+        mappedProps.loadEntitySet(id);
+
+        expect(dispatch).to.have.been.calledWith(ESDCActionFactory.entitySetDetailRequest(id));
+      });
+
+      it('should dispatch getEntitySetsAuthorizations', function() {
+        mappedProps.loadEntitySet(id);
+
+        expect(dispatch).to.have.been.calledWith(PermissionsActionFactory.getEntitySetsAuthorizations([id]));
+      });
+
+      it('should dispatch filteredEdmRequest', function() {
+        const data = [{
+          type: 'EntitySet',
+          id,
+          include: ['EntitySet', 'EntityType', 'PropertyTypeInEntitySet']
+        }];
+        mappedProps.loadEntitySet(id);
+
+        expect(dispatch).to.have.been.calledWith(EDMActionFactory.filteredEdmRequest(data));
+      });
+    });
+
+    describe('getAllUsersAndRolesRequest', function() {
+      it('should dispatch getAllUsersAndRolesRequest', function() {
+        mappedProps.actions.getAllUsersAndRolesRequest();
+
+        expect(dispatch).to.have.been.calledWith(PSActionFactory.getAllUsersAndRolesRequest());
+      });
+    });
+  });
+});
