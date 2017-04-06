2e3735f30726db5daec84bf92d8d344d37f63c5e
Corwin Crownover <corwin@thedataloom.com>
2017-04-05 17:33:56 -0700

    Organize by process

--- src/containers/permissionssummary/PermissionsSummaryReducer.test.js
+++ src/containers/permissionssummary/PermissionsSummaryReducer.test.js
@@ -47,184 +47,190 @@ describe('PermissionsSummaryReducer', function() {
     INITIAL_STATE = reducer(undefined, { type: 'init' });
   });
 
-  it('should update isGettingUsersRoles to true', function() {
-    const entitySet = {};
-    const action = PSActionFactory.getAllUsersAndRolesRequest(entitySet);
-    const state = reducer(INITIAL_STATE, action);
+  describe('Load users and roles', function() {
+    it('should update isGettingUsersRoles to true', function() {
+      const entitySet = {};
+      const action = PSActionFactory.getAllUsersAndRolesRequest(entitySet);
+      const state = reducer(INITIAL_STATE, action);
 
-    expect(state).to.have.property('isGettingUsersRoles').equal(true);
+      expect(state).to.have.property('isGettingUsersRoles').equal(true);
+    });
+
+    it('should update isGettingUsersRoles to false (success case)', function() {
+      const users = {};
+      const roles = [];
+      const action = PSActionFactory.getAllUsersAndRolesSuccess(users, roles);
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.property('isGettingUsersRoles').equal(false);
+    });
+
+    it('should update allUsersById', function() {
+      const users = Immutable.fromJS([{test: 'user1'}, {test: 'user2'}]);
+      const roles = Immutable.fromJS([{test: 'role1'}, {test: 'role2'}]);
+      const action = PSActionFactory.getAllUsersAndRolesSuccess(users, roles);
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.property('allUsersById').equal(users);
+    });
+
+    it('should update allRolesList', function() {
+      const users = Immutable.fromJS([{test: 'user1'}, {test: 'user2'}]);
+      const roles = Immutable.fromJS([{test: 'role1'}, {test: 'role2'}]);
+      const action = PSActionFactory.getAllUsersAndRolesSuccess(users, roles);
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.property('allRolesList').equal(roles);
+    });
+
+    it('should update isGettingUsersRoles to false (failure case)', function() {
+      const action = PSActionFactory.getAllUsersAndRolesFailure();
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.property('isGettingUsersRoles').equal(false);
+    });
   });
 
-  it('should update isGettingUsersRoles to false (success case)', function() {
-    const users = {};
-    const roles = [];
-    const action = PSActionFactory.getAllUsersAndRolesSuccess(users, roles);
-    const state = reducer(INITIAL_STATE, action);
+  describe('Load permissions', function() {
+    it('should update isGettingAcls to true', function() {
+      const entitySet = {};
+      const action = PSActionFactory.getAcls(entitySet);
+      const state = reducer(INITIAL_STATE, action);
 
-    expect(state).to.have.property('isGettingUsersRoles').equal(false);
+      expect(state).to.have.property('isGettingAcls').equal(true);
+    });
+
+    it('should update isGettingAcls to false', function() {
+      const entitySetId = '';
+      const property = {};
+      const action = PSActionFactory.getUserRolePermissionsRequest(entitySetId, property);
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.property('isGettingAcls').equal(false);
+    });
+
+    it('should update isGettingPermissions to true', function() {
+      const entitySetId = '';
+      const property = {};
+      const action = PSActionFactory.getUserRolePermissionsRequest(entitySetId, property);
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.property('isGettingPermissions').equal(true);
+    });
+
+    it('should update isGettingPermissions to false (success case)', function() {
+      const action = PSActionFactory.getUserRolePermissionsSuccess();
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.property('isGettingPermissions').equal(false);
+    });
+
+    it('should update isGettingPermissions to false (failure case)', function() {
+      const action = PSActionFactory.getUserRolePermissionsFailure();
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.property('isGettingPermissions').equal(false);
+    });
+
+    it('should get role permissions', function() {
+      const action = PSActionFactory.setRolePermissions(property, data);
+      const permissions = getRolePermissions(action);
+      const result = {
+        AuthenticatedUser: ['None'],
+        foo: ['Link']
+      };
+
+      expect(permissions).to.eql(result);
+    });
+
+    it('should update property role permissions', function() {
+      const action = PSActionFactory.setRolePermissions(property, data);
+      const permissions = Immutable.fromJS(getRolePermissions(action));
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.deep.property(`propertyPermissions.${property.title}.rolePermissions`).eql(permissions);
+    });
+
+    it('should update entity role permissions', function() {
+      const action = PSActionFactory.setRolePermissions(undefined, data);
+      const permissions = Immutable.fromJS(getRolePermissions(action));
+      const state = reducer(INITIAL_STATE, action);
+
+      expect(state).to.have.property('entityRolePermissions').eql(permissions);
+    });
+
+  // TODO: Fix errors in user permissions tests
+    // it('should get user permissions', function() {
+    //   const allUsersById = {
+    //     'auth0|foo': {
+    //       email: 'foo',
+    //       nickname: 'bar',
+    //       organization: [],
+    //       roles: ['foo'],
+    //       user_id: 'auth0|foo',
+    //       username: 'baz'
+    //     }
+    //   };
+    //   const action = PSActionFactory.setUserPermissions(property, data);
+    //   const permissions = getUserPermissions(action, Immutable.fromJS(allUsersById));
+    //   const result = [
+    //     {
+    //       email: 'foo',
+    //       id: 'auth0|foo',
+    //       individualPermissions: ['Write'],
+    //       nickname: 'bar',
+    //       permissions: ['Link', 'Write'],
+    //       roles: ['foo']
+    //     }
+    //   ];
+    //   console.log('permissions, result:', permissions, result);
+    //
+    //   expect(permissions).to.eql(result);
+    // });
+    //
+    // it('should update property user permissions', function() {
+    //   const allUsersById = {
+    //     'auth0|foo': {
+    //       email: 'foo',
+    //       nickname: 'bar',
+    //       organization: [],
+    //       roles: ['foo'],
+    //       user_id: 'auth0|foo',
+    //       username: 'baz'
+    //     }
+    //   };
+    //   const action = PSActionFactory.setUserPermissions(property, data);
+    //   const permissions = Immutable.fromJS(getUserPermissions(action, Immutable.fromJS(allUsersById)));
+    //   const state = reducer(INITIAL_STATE, action);
+    //
+    //   expect(state).to.have.deep.property(`propertyPermissions.${property.title}.userPermissions`).eql(permissions);
+    // });
+    //
+    // it('should update entity user permissions', function() {
+    //   const allUsersById = {
+    //     'auth0|foo': {
+    //       email: 'foo',
+    //       nickname: 'bar',
+    //       organization: [],
+    //       roles: ['foo'],
+    //       user_id: 'auth0|foo',
+    //       username: 'baz'
+    //     }
+    //   };
+    //   const action = PSActionFactory.setUserPermissions(undefined, data);
+    //   const permissions = Immutable.fromJS(getUserPermissions(action, Immutable.fromJS(allUsersById)));
+    //   const state = reducer(INITIAL_STATE, action);
+    //
+    //   expect(state).to.have.property('entityUserPermissions').eql(permissions);
+    // });
   });
 
-  it('should update allUsersById', function() {
-    const users = Immutable.fromJS([{test: 'user1'}, {test: 'user2'}]);
-    const roles = Immutable.fromJS([{test: 'role1'}, {test: 'role2'}]);
-    const action = PSActionFactory.getAllUsersAndRolesSuccess(users, roles);
-    const state = reducer(INITIAL_STATE, action);
+  describe('Reset permissions', function() {
+    it('should reset permissions', function() {
+      const action = PSActionFactory.resetPermissions();
+      const state = reducer(INITIAL_STATE, action);
 
-    expect(state).to.have.property('allUsersById').equal(users);
-  });
-
-  it('should update allRolesList', function() {
-    const users = Immutable.fromJS([{test: 'user1'}, {test: 'user2'}]);
-    const roles = Immutable.fromJS([{test: 'role1'}, {test: 'role2'}]);
-    const action = PSActionFactory.getAllUsersAndRolesSuccess(users, roles);
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.have.property('allRolesList').equal(roles);
-  });
-
-  it('should update isGettingUsersRoles to false (failure case)', function() {
-    const action = PSActionFactory.getAllUsersAndRolesFailure();
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.have.property('isGettingUsersRoles').equal(false);
-  });
-
-  it('should update isGettingAcls to true', function() {
-    const entitySet = {};
-    const action = PSActionFactory.getAcls(entitySet);
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.have.property('isGettingAcls').equal(true);
-  });
-
-  it('should update isGettingAcls to false', function() {
-    const entitySetId = '';
-    const property = {};
-    const action = PSActionFactory.getUserRolePermissionsRequest(entitySetId, property);
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.have.property('isGettingAcls').equal(false);
-  });
-
-  it('should update isGettingPermissions to true', function() {
-    const entitySetId = '';
-    const property = {};
-    const action = PSActionFactory.getUserRolePermissionsRequest(entitySetId, property);
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.have.property('isGettingPermissions').equal(true);
-  });
-
-  it('should update isGettingPermissions to false (success case)', function() {
-    const action = PSActionFactory.getUserRolePermissionsSuccess();
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.have.property('isGettingPermissions').equal(false);
-  });
-
-  it('should update isGettingPermissions to false (failure case)', function() {
-    const action = PSActionFactory.getUserRolePermissionsFailure();
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.have.property('isGettingPermissions').equal(false);
-  });
-
-  it('should get role permissions', function() {
-    const action = PSActionFactory.setRolePermissions(property, data);
-    const permissions = getRolePermissions(action);
-    const result = {
-      AuthenticatedUser: ['None'],
-      foo: ['Link']
-    };
-
-    expect(permissions).to.eql(result);
-  });
-
-  it('should update property role permissions', function() {
-    const action = PSActionFactory.setRolePermissions(property, data);
-    const permissions = Immutable.fromJS(getRolePermissions(action));
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.have.deep.property(`propertyPermissions.${property.title}.rolePermissions`).eql(permissions);
-  });
-
-  it('should update entity role permissions', function() {
-    const action = PSActionFactory.setRolePermissions(undefined, data);
-    const permissions = Immutable.fromJS(getRolePermissions(action));
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.have.property('entityRolePermissions').eql(permissions);
-  });
-
-// TODO: Fix errors in user permissions tests
-  // it('should get user permissions', function() {
-  //   const allUsersById = {
-  //     'auth0|foo': {
-  //       email: 'foo',
-  //       nickname: 'bar',
-  //       organization: [],
-  //       roles: ['foo'],
-  //       user_id: 'auth0|foo',
-  //       username: 'baz'
-  //     }
-  //   };
-  //   const action = PSActionFactory.setUserPermissions(property, data);
-  //   const permissions = getUserPermissions(action, Immutable.fromJS(allUsersById));
-  //   const result = [
-  //     {
-  //       email: 'foo',
-  //       id: 'auth0|foo',
-  //       individualPermissions: ['Write'],
-  //       nickname: 'bar',
-  //       permissions: ['Link', 'Write'],
-  //       roles: ['foo']
-  //     }
-  //   ];
-  //   console.log('permissions, result:', permissions, result);
-  //
-  //   expect(permissions).to.eql(result);
-  // });
-  //
-  // it('should update property user permissions', function() {
-  //   const allUsersById = {
-  //     'auth0|foo': {
-  //       email: 'foo',
-  //       nickname: 'bar',
-  //       organization: [],
-  //       roles: ['foo'],
-  //       user_id: 'auth0|foo',
-  //       username: 'baz'
-  //     }
-  //   };
-  //   const action = PSActionFactory.setUserPermissions(property, data);
-  //   const permissions = Immutable.fromJS(getUserPermissions(action, Immutable.fromJS(allUsersById)));
-  //   const state = reducer(INITIAL_STATE, action);
-  //
-  //   expect(state).to.have.deep.property(`propertyPermissions.${property.title}.userPermissions`).eql(permissions);
-  // });
-  //
-  // it('should update entity user permissions', function() {
-  //   const allUsersById = {
-  //     'auth0|foo': {
-  //       email: 'foo',
-  //       nickname: 'bar',
-  //       organization: [],
-  //       roles: ['foo'],
-  //       user_id: 'auth0|foo',
-  //       username: 'baz'
-  //     }
-  //   };
-  //   const action = PSActionFactory.setUserPermissions(undefined, data);
-  //   const permissions = Immutable.fromJS(getUserPermissions(action, Immutable.fromJS(allUsersById)));
-  //   const state = reducer(INITIAL_STATE, action);
-  //
-  //   expect(state).to.have.property('entityUserPermissions').eql(permissions);
-  // });
-
-  it('should reset permissions', function() {
-    const action = PSActionFactory.resetPermissions();
-    const state = reducer(INITIAL_STATE, action);
-
-    expect(state).to.equal(INITIAL_STATE);
+      expect(state).to.equal(INITIAL_STATE);
+    });
   });
 });
