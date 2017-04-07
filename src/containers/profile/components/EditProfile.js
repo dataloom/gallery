d3924ba8fa88a13044cf9150616c5660847e06d0
Corwin Crownover <corwin@thedataloom.com>
2017-04-07 16:25:55 -0700

    Add document title and page header

--- src/containers/profile/components/EditProfile.js
+++ src/containers/profile/components/EditProfile.js
@@ -1,14 +1,21 @@
 import React, { PropTypes } from 'react';
 import Immutable from 'immutable';
 import { Button } from 'react-bootstrap';
+import DocumentTitle from 'react-document-title';
+import Page from '../../../components/page/Page';
 import styles from '../styles.module.css';
 
-const EditProfile = ({onSubmit}) => {
-  return(
-    <div>
-      <div>EditProfileComponent!</div>
-      <Button onClick={onSubmit}>Submit</Button>
-    </div>
+const EditProfile = ({ userDetails, onSubmit }) => {
+  return (
+    <DocumentTitle title="Edit Profile">
+      <Page>
+        <Page.Header>
+          <Page.Title>Edit Profile</Page.Title>
+        </Page.Header>
+        <div>EditProfileComponent!</div>
+        <Button onClick={onSubmit}>Submit</Button>
+      </Page>
+    </DocumentTitle>
   );
 };
 
