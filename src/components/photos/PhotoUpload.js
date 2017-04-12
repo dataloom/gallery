393a0d3a16af68a36e9cda87227af89375e0b46e
Corwin Crownover <corwin@thedataloom.com>
2017-04-12 13:20:09 -0700

    Add placeholder onClick method

--- src/components/photos/PhotoUpload.js
+++ src/components/photos/PhotoUpload.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap';
 import StyledFlexContainer from '../flex/StyledFlexContainer';
 import styles from './styles.module.css';
 
-const PhotoUpload = ({ header, content }) => {
+const PhotoUpload = ({ header, content, onClick }) => {
   return (
     <div className={styles.photoUploadWrapper}>
       <div className={styles.header}>
@@ -14,7 +14,7 @@ const PhotoUpload = ({ header, content }) => {
         <div className={styles.photoWrapper}>
           {content}
         </div>
-        <Button className={styles.uploadButton}>Upload photo
+        <Button className={styles.uploadButton} onClick={onClick}>Upload photo
         </Button>
       </div>
     </div>
@@ -23,7 +23,8 @@ const PhotoUpload = ({ header, content }) => {
 
 PhotoUpload.propTypes = {
   header: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired
+  content: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
 };
 
 export default PhotoUpload;
