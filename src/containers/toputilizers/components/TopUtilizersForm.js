01d1f201cd4a232bdfafa80eac9414119ee0a790
Corwin Crownover <corwin@thedataloom.com>
2017-04-18 13:45:09 -0700

    Get entity sets

--- src/containers/toputilizers/components/TopUtilizersForm.js
+++ src/containers/toputilizers/components/TopUtilizersForm.js
@@ -2,11 +2,14 @@ import React from 'react';
 import DocumentTitle from 'react-document-title';
 
 import Page from '../../../components/page/Page.js';
+import ContentSection from '../../../components/applayout/ContentSection';
 import styles from '../styles.module.css';
 
 const TopUtilizersForm = ({}) => {
   return (
-    <div>top utilizers</div>
+    <ContentSection>
+      hey
+    </ContentSection>
   );
 };
 
--- src/containers/toputilizers/containers/TopUtilizersFormContainer.js
+++ src/containers/toputilizers/containers/TopUtilizersFormContainer.js
@@ -4,12 +4,25 @@ import { bindActionCreators } from 'redux';
 import Immutable from 'immutable';
 
 import * as actionFactory from '../TopUtilizersActionFactory';
-import { entitySetDetailRequest } from '../../entitysetdetail/EntitySetDetailActionFactories';
 import TopUtilizersForm from '../components/TopUtilizersForm';
+import { allEntitySetsRequest } from '../../catalog/CatalogActionFactories';
+import { getShallowEdmObjectSilent } from '../../edm/EdmStorage';
+
 
 class TopUtilizersFormContainer extends React.Component {
   static propTypes = {
     entitySetId: PropTypes.string.isRequired
+  }
+
+  componentDidMount() {
+    this.props.allEntitySetsRequest();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps && nextProps.entitySets) {
+      console.log('nextprops entitysets:', nextProps.entitySets.toJS());
+
+    }
   }
 
   render() {
@@ -21,15 +34,27 @@ class TopUtilizersFormContainer extends React.Component {
 
 function mapStateToProps(state) {
   const topUtilizers = state.get('topUtilizers');
+  const catalog = state.get('catalog');
+  const normalizedData = state.get('normalizedData').toJS();
+
+  let entitySets = [];
+  if (catalog && catalog.get('allEntitySetReferences')) {
+    entitySets = catalog.get('allEntitySetReferences').map((reference) => {
+      return getShallowEdmObjectSilent(normalizedData, reference, null);
+    }).filter((entitySet) => {
+      return entitySet;
+    });
+  }
 
   return {
-    entitySetId: topUtilizers.get('entitySetId')
+    entitySetId: topUtilizers.get('entitySetId'),
+    entitySets
   };
 }
 
-function mapDispatchToProps(dispatch, ownProps) {
+function mapDispatchToProps(dispatch) {
   const actions = {
-
+    allEntitySetsRequest
   };
 
   return bindActionCreators(actions, dispatch);
