581e199b211a1bcf7a3891c93c67ff1560c195a7
Katherine <katherine@kryptnostic.com>
2017-04-24 15:39:17 -0700

    render count column in top utilizers results

--- src/containers/entitysetsearch/EntitySetSearchResults.js
+++ src/containers/entitysetsearch/EntitySetSearchResults.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { Table, Column, Cell } from 'fixed-data-table';
 import EntityRow from './EntityRow';
 import TextCell from './TextCell';
+import styles from './styles.module.css';
 
 const TABLE_WIDTH = 1000;
 const MAX_TABLE_HEIGHT = 500;
@@ -13,7 +14,8 @@ export default class EntitySetSearchResults extends React.Component {
     results: PropTypes.array.isRequired,
     entitySetId: PropTypes.string.isRequired,
     propertyTypes: PropTypes.array.isRequired,
-    formatValueFn: PropTypes.func.isRequired
+    formatValueFn: PropTypes.func.isRequired,
+    showCount: PropTypes.boolean
   }
 
   constructor(props) {
@@ -48,11 +50,30 @@ export default class EntitySetSearchResults extends React.Component {
 
   renderColumns = () => {
     const columnWidth = (TABLE_WIDTH - 1) / this.props.propertyTypes.length;
-    return this.props.propertyTypes.map((propertyType) => {
+    const columns = [];
+    if (this.props.showCount) {
+      columns.push(
+        <Column
+            key="count"
+            header={<Cell className={styles.countHeaderCell}>Count</Cell>}
+            cell={
+              <TextCell
+                  results={this.state.results}
+                  field="count"
+                  formatValueFn={this.props.formatValueFn}
+                  onClick={this.onRowSelect}
+                  width={columnWidth}
+                  entitySetId={this.props.entitySetId}
+                  propertyTypes={this.props.propertyTypes} />
+            }
+            width={columnWidth} />
+      );
+    }
+    this.props.propertyTypes.forEach((propertyType) => {
       const key = (Object.keys(this.state.results[0])[0].indexOf('.') > -1)
         ? `${propertyType.type.namespace}.${propertyType.type.name}`
         : propertyType.id;
-      return (
+      columns.push(
         <Column
             key={key}
             header={<Cell>{propertyType.title}</Cell>}
@@ -69,6 +90,7 @@ export default class EntitySetSearchResults extends React.Component {
             width={columnWidth} />
       );
     });
+    return columns;
   }
 
   renderSingleRow = () => {
