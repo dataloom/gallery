2dc291ed7d2da656d17adc59f56f8b996334d0e5
Corwin Crownover <corwin@thedataloom.com>
2017-04-27 12:32:05 -0700

    Fix proptype errors

--- src/containers/entitysetdetail/IntegrationDetailsModal.js
+++ src/containers/entitysetdetail/IntegrationDetailsModal.js
@@ -16,7 +16,7 @@ const IntegrationDetailsModal = ({ isOpen, onClose, entitySet }) => {
           namespace = property.type.namespace;
         }
       });
-      return (<li>{namespace}.{name}</li>);
+      return (<li key={key}>{namespace}.{name}</li>);
     });
   };
 
@@ -42,7 +42,7 @@ const IntegrationDetailsModal = ({ isOpen, onClose, entitySet }) => {
   const getPropertyListItems = () => {
     const { properties } = entitySet.entityType;
     return properties.map((property) => (
-      <li><b>{property.title}: </b>{property.type.namespace}.{property.type.name}</li>
+      <li key={property.id}><b>{property.title}: </b>{property.type.namespace}.{property.type.name}</li>
     ));
   };
 
@@ -77,7 +77,7 @@ const IntegrationDetailsModal = ({ isOpen, onClose, entitySet }) => {
 IntegrationDetailsModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
-  entitySet: PropTypes.object.isRequired
+  entitySet: PropTypes.object
 };
 
 export default IntegrationDetailsModal;
