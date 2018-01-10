e327b494f709b047c77a84359072167e00097f51
Corwin Crownover <corwin.crownover@gmail.com>
2018-01-10 14:37:37 +0800

    Update property access for user b/c not an immutable

--- src/containers/permissionssummary/PermissionsSummaryHelpers.js
+++ src/containers/permissionssummary/PermissionsSummaryHelpers.js
@@ -20,67 +20,77 @@ export function getRolePermissions(action) {
   });
 
   rolePermissions[AUTHENTICATED_USER] = authenticatedUserPermissions.length === 0 ? [NONE] : authenticatedUserPermissions;
+  console.log('rolePermissions from helper fn:', rolePermissions);
   return rolePermissions;
 }
 
 export function getUserPermissions(action, allUsersById) {
+  console.log('getUserPermissions is getting called, action & allUsersById:', action, allUsersById);
+  // WTF DO all users get passed in, is there a better way to reference, e.g. a fn w/ clouser?
   const { userAcls, roleAcls, authenticatedUserPermissions } = action.data;
   const userPermissions = [];
+  try {
+    allUsersById.valueSeq().forEach((user) => {
+      let userId;
+      let nickname;
+      let email;
 
-  allUsersById.valueSeq().forEach((user) => {
-    let userId;
-    let nickname;
-    let email;
+      if (user) {
+        userId = user.user_id;
+        nickname = user.nickname;
+        email = user.email;
 
-    if (user) {
-      userId = user.get('user_id');
-      nickname = user.get('nickname');
-      email = user.get('email');
+        const userObj = {
+          id: userId,
+          nickname,
+          email,
+          roles: [],
+          permissions: [],
+          individualPermissions: []
+        };
 
-      const userObj = {
-        id: userId,
-        nickname,
-        email,
-        roles: [],
-        permissions: [],
-        individualPermissions: []
-      };
-
-      // Get all user permissions (sum of individual + roles + default);
-      Object.keys(userAcls).forEach((permissionKey) => {
-        if (userAcls[permissionKey].indexOf(userId) !== -1) {
-          userObj.permissions.push(permissionKey);
-          // Save individual permissions separately
-          userObj.individualPermissions.push(permissionKey);
-        }
-      });
-
-      // Add additional permissions based on the roles the user has
-      const roles = user.get('roles');
-      if (roles.size > 1) {
-        Object.keys(roleAcls).forEach((permissionKey) => {
-          roles.forEach((role) => {
-            if (roleAcls[permissionKey].indexOf(role) !== -1 && userObj.permissions.indexOf(permissionKey) === -1) {
-              userObj.permissions.push(permissionKey);
-            }
-            if (userObj.roles.indexOf(role) === -1 && role !== AUTHENTICATED_USER) {
-              userObj.roles.push(role);
-            }
-          });
-        });
-      }
-
-      // Add additional permissions based on default for all users
-      if (authenticatedUserPermissions) {
-        authenticatedUserPermissions.forEach((permission) => {
-          if (userObj.permissions.indexOf(permission) === -1) {
-            userObj.permissions.push(permission);
+        // Get all user permissions (sum of individual + roles + default);
+        Object.keys(userAcls).forEach((permissionKey) => {
+          if (userAcls[permissionKey].indexOf(userId) !== -1) {
+            userObj.permissions.push(permissionKey);
+            // Save individual permissions separately
+            userObj.individualPermissions.push(permissionKey);
           }
         });
-      }
 
-      userPermissions.push(userObj);
-    }
-  });
+        // Add additional permissions based on the roles the user has
+        const roles = user.roles;
+        if (roles.size > 1) {
+          Object.keys(roleAcls).forEach((permissionKey) => {
+            roles.forEach((role) => {
+              if (roleAcls[permissionKey].indexOf(role) !== -1 && userObj.permissions.indexOf(permissionKey) === -1) {
+                userObj.permissions.push(permissionKey);
+              }
+              if (userObj.roles.indexOf(role) === -1 && role !== AUTHENTICATED_USER) {
+                userObj.roles.push(role);
+              }
+            });
+          });
+        }
+
+        // Add additional permissions based on default for all users
+        if (authenticatedUserPermissions) {
+          authenticatedUserPermissions.forEach((permission) => {
+            if (userObj.permissions.indexOf(permission) === -1) {
+              userObj.permissions.push(permission);
+            }
+          });
+        }
+
+        userPermissions.push(userObj);
+      }
+    });
+  }
+  catch(e) {
+    console.log('error:', e);
+  }
+  
+  console.log('userPermissions from helper fn:', userPermissions);
+
   return userPermissions;
 }
