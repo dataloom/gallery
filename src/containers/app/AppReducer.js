85999c7d0fc5a7cf25686e1dbc0e3664dcc51901
Marie Reed <marie@openlattice.com>
2018-03-12 12:05:47 -0700

    Created App reducer.

--- src/containers/app/AppReducer.js
+++ src/containers/app/AppReducer.js
@@ -1,10 +1,14 @@
 import Immutable from 'immutable';
-
+import { ASYNC_STATUS } from '../../components/asynccontent/AsyncContent';
 import * as actionTypes from './AppActionTypes';
 
 export const INITIAL_STATE = Immutable.fromJS({
   apps: Immutable.List(),
-  errorMessage: ''
+  errorMessage: '',
+  createAppAsyncState: {
+    status: ASYNC_STATUS.PENDING,
+    errorMessage: ''
+  }
 });
 
 export default function reducer(state = INITIAL_STATE, action) {
@@ -23,6 +27,42 @@ export default function reducer(state = INITIAL_STATE, action) {
 
     case actionTypes.INSTALL_APP_FAILURE:
       return state.set('errorMessage', action.errorMessage);
+
+    case actionTypes.CREATE_APP_REQUEST:
+    case actionTypes.CREATE_APP_TYPE_REQUEST:
+      return state.mergeDeep({
+        createAppAsyncState: {
+          status: ASYNC_STATUS.LOADING,
+          errorMessage: ''
+        }
+      });
+
+    case actionTypes.CREATE_APP_REJECT:
+    case actionTypes.CREATE_APP_TYPE_REJECT:
+      return state.mergeDeep({
+        createAppAsyncState: {
+          status: ASYNC_STATUS.ERROR,
+          errorMessage: action.errorMessage
+        }
+      });
+
+    case actionTypes.CREATE_APP_RESOLVE:
+    case actionTypes.CREATE_APP_TYPE_RESOLVE:
+      return state.mergeDeep({
+        createAppAsyncState: {
+          status: ASYNC_STATUS.SUCCESS,
+          errorMessage: ''
+        }
+      });
+
+    case actionTypes.CREATE_APP_RESET:
+    case actionTypes.CREATE_APP_TYPE_RESET:
+      return state.mergeDeep({
+        createAppAsyncState: {
+          status: ASYNC_STATUS.PENDING,
+          errorMessage: ''
+        }
+      });
 
     default:
       return state;
