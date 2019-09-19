c7e52c03f2a2f91258e293f34d44e7b88e928cdb
Drew <drew@openlattice.com>
2019-09-19 16:01:59 -0400

    Include default select statements Escape quotes in gluttony string Include subquery parentheses and table name Add connection suffix Add default ports

--- src/containers/organizations/utils/IntegrationYamlUtils.js
+++ src/containers/organizations/utils/IntegrationYamlUtils.js
@@ -6,122 +6,170 @@ export const DATA_SQL_TYPES = {
   'Pervasive SQL': {
     driver: 'com.pervasive.jdbc.v2.Driver',
     connectionString: 'pervasive',
-    tablesSql: '( SELECT * FROM MasterNameVitals ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM MasterNameVitals',
+    defaultPort: 0
   },
   'IBM DB2': {
     driver: 'COM.ibm.db2.jdbc.app.DB2Driver',
     connectionString: 'db2',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'JDBC-ODBC Bridge': {
     driver: 'sun.jdbc.odbc.JdbcOdbcDriver',
     connectionString: 'odbc',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Microsoft SQL Server': {
     driver: 'weblogic.jdbc.mssqlserver4.Driver',
     connectionString: 'weblogic',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'PointBase Embedded Server': {
     driver: 'com.pointbase.jdbc.jdbcUniversalDriver',
     connectionString: 'pointbase',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Cloudscape': {
     driver: 'COM.cloudscape.core.JDBCDriver',
     connectionString: 'cloudscape',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Cloudscape RMI': {
     driver: 'RmiJdbc.RJDriver',
     connectionString: 'rmi',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Firebird (JCA/JDBC Driver)': {
     driver: 'org.firebirdsql.jdbc.FBDriver',
     connectionString: 'firebirdsql',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'IDS Server': {
     driver: 'ids.sql.IDSDriver',
     connectionString: 'ids',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Informix Dynamic Server': {
     driver: 'com.informix.jdbc.IfxDriver',
     connectionString: 'informix-sqli',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: ':informixserver=<serverNameHere>;DELIMIDENT=Y',
+    tablesSql: 'INFO TABLES',
+    defaultPort: 0
   },
   'InstantDB (v3.13 and earlier)': {
     driver: 'jdbc.idbDriver',
     connectionString: 'idb',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'InstantDB (v3.14 and later)': {
     driver: 'org.enhydra.instantdb.jdbc.idbDriver',
     connectionString: 'idb',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Interbase (InterClient Driver)': {
     driver: 'interbase.interclient.Driver',
     connectionString: 'interbase',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Hypersonic SQL (v1.2 and earlier)': {
     driver: 'hSql.hDriver',
     connectionString: 'HypersonicSQL',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Hypersonic SQL (v1.3 and later)': {
     driver: 'org.hsql.jdbcDriver',
     connectionString: 'HypersonicSQL',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Microsoft SQL Server (JTurbo Driver)': {
     driver: 'com.ashna.jturbo.driver.Driver',
     connectionString: 'JTurbo',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Microsoft SQL Server (Sprinta Driver)': {
     driver: 'com.inet.tds.TdsDriver',
     connectionString: 'inetdae',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Microsoft SQL Server 2000 (Microsoft Driver)': {
     driver: 'com.microsoft.jdbc.sqlserver.SQLServerDriver',
     connectionString: 'microsoft',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'MySQL (MM.MySQL Driver)': {
     driver: 'org.gjt.mm.mysql.Driver',
     connectionString: 'mysql',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   Oracle: {
     driver: 'oracle.jdbc.driver.OracleDriver',
     connectionString: 'oracle',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'PostgreSQL (v6.5 and earlier)': {
     driver: 'postgresql.Driver',
     connectionString: 'postgresql',
-    tablesSql: '( SELECT * FROM pg_catalog.pg_tables ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM pg_catalog.pg_tables',
+    defaultPort: 5432
   },
   'PostgreSQL (v7.0 and later)': {
     driver: 'org.postgresql.Driver',
     connectionString: 'postgresql',
-    tablesSql: '( SELECT * FROM pg_catalog.pg_tables ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM pg_catalog.pg_tables',
+    defaultPort: 5432
   },
   'Sybase (jConnect 4.2 and earlier)': {
     driver: 'com.sybase.jdbc.SybDriver',
     connectionString: 'sybase',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   },
   'Sybase (jConnect 5.2)': {
     driver: 'com.sybase.jdbc2.jdbc.SybDriver',
     connectionString: 'sybase',
-    tablesSql: '( SELECT * FROM <metaTableGoesHere> ) dh'
+    connectionSuffixString: '',
+    tablesSql: 'SELECT * FROM <metaTableGoesHere>',
+    defaultPort: 0
   }
 };
 
@@ -135,12 +183,14 @@ export const exportTemplate = ({
   orgUsername,
   orgPassword
 }) => {
-  const { driver, connectionString, tablesSql } = DATA_SQL_TYPES[dataSqlType];
+  const { driver, connectionString, connectionSuffixString, tablesSql, defaultPort } = DATA_SQL_TYPES[dataSqlType];
   const orgIdShort = `org_${orgId.replace(/-/g, '')}`;
   const orgNameShort = orgName.replace(/[^a-zA-Z0-9]/g, '');
 
   const fieldMappings = {
     [TEMPLATE_CONSTANTS.DATA_SQL_TYPE]: connectionString,
+    [TEMPLATE_CONSTANTS.DEFAULT_PORT]: defaultPort,
+    [TEMPLATE_CONSTANTS.CONNECTION_SUFFIX_STRING]: connectionSuffixString,
     [TEMPLATE_CONSTANTS.SQL_DRIVER_STRING]: driver,
     [TEMPLATE_CONSTANTS.TABLE_LISTING_SQL]: tablesSql,
     [TEMPLATE_CONSTANTS.TARGET_SERVER]: server,
